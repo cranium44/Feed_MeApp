793688331f6d156d5ae2f59755250d102797a6c6
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-24 02:28:48 +0100

    fixed one time onboarding

--- app/src/main/java/com/decagon/feedme/view/OnboardingActivity.kt
+++ app/src/main/java/com/decagon/feedme/view/OnboardingActivity.kt
@@ -1,7 +1,10 @@
 package com.decagon.feedme.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.decagon.feedme.MainActivity
 import com.decagon.feedme.R
 import com.decagon.feedme.controllers.OnBoardingAdapter
 import kotlinx.android.synthetic.main.activity_onboarding.*
@@ -13,5 +16,21 @@ class OnboardingActivity: AppCompatActivity() {
         setContentView(R.layout.activity_onboarding)
 
         view_pager.adapter = OnBoardingAdapter(this)
+        indicator.setViewPager(view_pager)
+
+        on_nxt_button.setOnClickListener {
+            setOnboarded()
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    private fun setOnboarded() {
+        val sharedPreferences =
+            getSharedPreferences(MainActivity.SHARED_PREF_CONFIG, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(MainActivity.ONBOARDED, true).apply()
+    }
+
+
 }
