529fcd14bb01f3ca19352c5afcf4065fe87a431b
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-24 01:19:45 +0100

    completed onboarding enum class

--- app/src/main/java/com/decagon/feedme/models/OnboardingEnum.kt
+++ app/src/main/java/com/decagon/feedme/models/OnboardingEnum.kt
@@ -1,4 +1,12 @@
 package com.decagon.feedme.models
 
-enum class OnboardingEnum {
+import com.decagon.feedme.R
+
+enum class OnboardingEnum(onboardingScreenEat: Int) {
+
+    COOK(R.layout.onboarding_screen_cook),
+    MEET(R.layout.onboarding_screen_meet),
+    EAT(R.layout.onboarding_screen_eat);
+
+
 }
