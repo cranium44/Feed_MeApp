e32b8c73442da7b7baed1fedb8e1ea537b51b083
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-25 23:35:49 +0100

    created sign up activity class and layout file

---
+++ app/src/main/java/com/decagon/feedme/view/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.decagon.feedme.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.decagon.feedme.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
