9289a38dd67b635e5a12d777c951dd3d331a994a
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-03-03 03:10:14 +0100

    added permissions library

--- app/src/main/java/com/decagon/feedme/view/AddRecipeFragment.kt
+++ app/src/main/java/com/decagon/feedme/view/AddRecipeFragment.kt
@@ -1,15 +1,28 @@
 package com.decagon.feedme.view
 
-import androidx.lifecycle.ViewModelProviders
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.NumberPicker
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.decagon.feedme.R
 import com.decagon.feedme.viewmodels.AddRecipeViewModel
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+
+
 
 class AddRecipeFragment : Fragment() {
 
@@ -45,6 +58,49 @@ class AddRecipeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(AddRecipeViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun requestForStoragePermission(){
+        Dexter.withActivity(this.activity)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(object : PermissionListener{
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+            })
+    }
+
+    private fun showSettingsDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this.context)
+        builder.setTitle("Need Permissions")
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS",
+            DialogInterface.OnClickListener { dialog, which ->
+                dialog.cancel()
+                openSettings()
+            })
+        builder.setNegativeButton("Cancel",
+            DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+        builder.show()
+    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", this.activity?.packageName,null)
+        intent.data = uri
+        startActivityForResult(intent, 101)
     }
 
 }
